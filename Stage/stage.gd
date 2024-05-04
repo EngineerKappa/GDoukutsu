72b5c1dc2e33f80c7707c4fb04ba455219ebff14
EngineerKappa <engineerkappa@live.com>
2024-05-04 15:13:26 -0400

    Parallax

--- Stage/stage.gd
+++ Stage/stage.gd
@@ -1,6 +1,7 @@
 class_name Stage extends Node2D
 @export var map_name:String = "Room"
 @export_file("*.ogg") var bgm
+@onready var label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
